f7834fe76526de7f0b1933c532a7d8feb88f5a85
joshuadorley <joshuaboateng33@gmail.com>
2024-09-07 21:48:36 -0400

    JSSelect

--- app.js
+++ app.js
@@ -2,3 +2,113 @@ console.log("Hello World!\n==========\n");
 console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
+
+// Initialize a favorite number
+let favoriteNumber = 42;
+
+// Ask the user to input a number using window.prompt
+let userGuess = parseInt(prompt("Guess the favorite number:"));
+
+// Create an if-else structure to compare the user's guess
+if (userGuess < favoriteNumber) {
+  console.log("Too low!");
+} else if (userGuess > favoriteNumber) {
+  console.log("Too high!");
+} else {
+  console.log("Congratulations!!! You guessed it right.");
+}
+
+// Exercise 2
+let birthMonth = prompt("Please insert your birth month");
+
+switch (birthMonth) {
+  case "December":
+  case "January":
+  case "February":
+    console.log("Winter");
+    break; 
+  case "March":
+  case "April": 
+  case "May":
+      console.log("Spring");
+      break;
+  case "June":
+  case "July": 
+  case "August":
+        console.log("Summer");
+        break;
+  case "September":
+  case "October":
+  case "November":
+          console.log("Fall");
+          break;
+        default:
+          console.log("I don't recognize that month...")
+}
+
+//Exercise 3
+let typeId = "01";
+let colorId = "PU";
+let sizeId = "L";
+
+let type = "";
+let color = "";
+let size = "";
+
+
+switch (typeId) {
+  case "01":
+    type = "Tank top";
+    break; 
+  case "02":
+      type = "T-shirt";
+    break;
+  case "03":
+    type = "Long Sleeve";
+    break;
+  case "04":
+    type = "Sweat Shirt";
+    break;
+  default:
+    type = "Other";    
+
+}
+
+
+switch (colorId) {
+  case "BL":
+    color = "Black";
+    break;
+  case "BU":
+    color = "Blue";
+    break;
+  case "RD":
+    color = "Red";
+    break;
+  case "PU":
+    color = "Purple";
+    break;
+  default:
+    color = "White"     
+
+}
+
+
+switch (sizeId) {
+  case "S":
+    size = "Small";
+    break;
+  case "M":
+    size = "Medium";
+    break;
+  case "L":
+    size = "Large";
+    break;
+  case "XL":
+    size = "Extra Large";
+    break;
+  default:
+    size = "One Size Fits All";        
+}
+
+console.log(`Product: ${size} ${color} ${type}`);
